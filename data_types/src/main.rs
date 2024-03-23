21378c578097c725796971833025876d8b86da6d
arshkhatwani <arsh.khatwani@gmail.com>
2024-03-23 14:14:10 +0530

    Vectors

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -40,12 +40,33 @@ fn main() {
     // let (x, y, z) = tup;
     // println!("{} {} {}", x, y, z);
 
-    let array = [1, 2, 3];
-    println!("{:?}", array);
-    println!("{}", array[1]);
+    // let array = [1, 2, 3];
+    // println!("{:?}", array);
+    // println!("{}", array[1]);
 
-    let mut array: [i32; 4] = [10, 20, 30, 40];
-    println!("{}", array[2]);
-    array[2] = 15;
-    println!("{}", array[2]);
+    // let mut array: [i32; 4] = [10, 20, 30, 40];
+    // println!("{}", array[2]);
+    // array[2] = 15;
+    // println!("{}", array[2]);
+
+    let mut nums = vec![1, 2, 3]; // vect! macro
+    println!("{:?}", nums);
+
+    nums.push(23);
+    nums[1] = 32;
+    println!("{:?}", nums);
+
+    let mut vec = Vec::new(); // same as using vec!
+    vec.push("text");
+    vec.push("string");
+    println!("{:?}", vec);
+
+    vec.reverse();
+    println!("{:?}", vec);
+
+    let vect = Vec::<i32>::with_capacity(2);
+    println!("capacity of vect: {}", vect.capacity());
+
+    let v: Vec<i32> = (0..5).collect();
+    println!("{:?}", v);
 }

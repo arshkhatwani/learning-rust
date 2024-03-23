5e10d5f0171f2355fea5716dd241c4f009dc735b
arshkhatwani <arsh.khatwani@gmail.com>
2024-03-23 19:11:43 +0530

    Control Flow

---
+++ control_flow/src/main.rs
@@ -0,0 +1,34 @@
+fn main() {
+    // let mut num = 5;
+    // 'counter: loop {
+    //     println!("Count: {}", num);
+    //     let mut decrease = 5;
+    //     loop {
+    //         println!("Decreasing: {}", decrease);
+    //         if decrease == 4 {
+    //             break;
+    //         }
+    //         if num == 2 {
+    //             break 'counter;
+    //         }
+    //         decrease -= 1;
+    //     }
+    //     num -= 1;
+    // }
+
+    // let mut num: i32 = 0;
+    // while num <= 5 {
+    //     println!("num is {}", num);
+    //     num += 1;
+    // }
+
+    // let vec: Vec<i8> = (0..5).collect();
+    // for element in vec {
+    //     println!("{}", element);
+    // }
+
+    // Reverse loop
+    for num in (1..4).rev() {
+        println!("{}", num);
+    }
+}

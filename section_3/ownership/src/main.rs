398d7aff773a7b927a5e1c40e343767279ec133c
arshkhatwani <arsh.khatwani@gmail.com>
2024-03-24 17:28:22 +0530

    Move and Clone

--- section_3/ownership/src/main.rs
+++ section_3/ownership/src/main.rs
@@ -1,10 +1,31 @@
 fn main() {
-    let var1 = 10; // on the stack
-    println!("{}", var1);
+    // ---Ownership---
+    // let var1 = 10; // on the stack
+    // println!("{}", var1);
 
-    let mut s = "hello".to_string(); // on the heap
-    s.push_str(", world");
-    println!("{}", s);
+    // let mut s = "hello".to_string(); // on the heap
+    // s.push_str(", world");
+    // println!("{}", s);
+
+    // ---Move---
+    // let x = vec!["hello".to_string()];
+    // let y = x; // value of x is moved
+
+    // // println!("x: {:?}", x); // won't work since value of x is moved, see compiler error for more details
+    // println!("y: {:?}", y);
+
+    // // now similar would happen in case of y
+    // let z = y;
+    // // println!("y: {:?}", y); // won't work
+    // println!("z: {:?}", z);
+
+    // ---Clone---
+    let x = vec!["hello".to_string()];
+    let y = x.clone();
+    let z = y.clone();
+    println!("x: {:?}", x);
+    println!("y: {:?}", y);
+    println!("z: {:?}", z);
 }
 
 // var is dropped, s is dropped

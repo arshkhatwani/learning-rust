0eb7824df3f46f18c2b3f374b2afe71d2ce7aeff
arshkhatwani <arsh.khatwani@gmail.com>
2024-03-24 19:08:21 +0530

    References and Borrowing

--- section_3/ownership/src/main.rs
+++ section_3/ownership/src/main.rs
@@ -32,37 +32,47 @@ fn main() {
     // let y = x;
     // println!("x: {} y: {}", x, y);
 
-    let s: String = String::from("takes");
-    takes_ownership(s);
-    // println!("{}", s); // won't work
+    // let s: String = String::from("takes");
+    // takes_ownership(s);
+    // // println!("{}", s); // won't work
 
-    let val: i32 = 10;
-    make_copy(val);
-    println!("{}", val);
+    // let val: i32 = 10;
+    // make_copy(val);
+    // println!("{}", val);
 
-    let t = give_ownership();
-    println!("{}", t);
+    // let t = give_ownership();
+    // println!("{}", t);
 
-    let s2 = take_and_give("world".to_string());
-    println!("{}", s2);
+    // let s2 = take_and_give("world".to_string());
+    // println!("{}", s2);
+
+    // ---References and Borrowing---
+    let mut s = String::from("hello");
+    change_str(&mut s);
+    println!("{}", s);
+    // Here we were able to change the value without taking ownership inside the function, if we had not passed by reference then 'change_str' would have taken ownership of s hence we would not be able to use 's' after function
 }
 
-fn takes_ownership(s: String) {
-    let new_str = s;
-    println!("{}", new_str);
+fn change_str(s: &mut String) {
+    s.push_str(", world");
 }
 
-fn make_copy(val: i32) {
-    let cpy_val = val;
-    println!("{}", cpy_val);
-}
+// fn takes_ownership(s: String) {
+//     let new_str = s;
+//     println!("{}", new_str);
+// }
 
-fn give_ownership() -> String {
-    "hello".to_string()
-}
+// fn make_copy(val: i32) {
+//     let cpy_val = val;
+//     println!("{}", cpy_val);
+// }
 
-fn take_and_give(s: String) -> String {
-    s
-}
+// fn give_ownership() -> String {
+//     "hello".to_string()
+// }
+
+// fn take_and_give(s: String) -> String {
+//     s
+// }
 
 // var is dropped, s is dropped

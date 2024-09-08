c349f7009fc97ad48c3b0c855a2c67f6acf1340f
arshkhatwani <arsh.khatwani@gmail.com>
2024-09-09 01:46:20 +0530

    Sets

--- section_8/src/main.rs
+++ section_8/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{BinaryHeap, HashMap};
+use std::collections::{BinaryHeap, HashMap, HashSet};
 
 use rand::seq::SliceRandom;
 
@@ -40,27 +40,63 @@ fn main() {
     // let el = bheap.peek();
     // println!("{:?}", el);
 
-    let mut hm = HashMap::new();
+    // let mut hm = HashMap::new();
 
-    hm.insert(2, 3);
-    hm.insert(3, 6);
-    let old = hm.insert(2, 5);
-    hm.insert(10, 8);
+    // hm.insert(2, 3);
+    // hm.insert(3, 6);
+    // let old = hm.insert(2, 5);
+    // hm.insert(10, 8);
 
-    println!("{:?}", hm);
-    println!("old val: {:?}", old);
+    // println!("{:?}", hm);
+    // println!("old val: {:?}", old);
 
-    println!("has key? {}", hm.contains_key(&10));
-    println!("has key? {}", hm.contains_key(&100));
-    println!("find {:?}", hm.get(&10));
-    println!("find {:?}", hm.get(&100));
+    // println!("has key? {}", hm.contains_key(&10));
+    // println!("has key? {}", hm.contains_key(&100));
+    // println!("find {:?}", hm.get(&10));
+    // println!("find {:?}", hm.get(&100));
 
-    let one = hm.remove(&2);
-    println!("removed: {:?}", one);
+    // let one = hm.remove(&2);
+    // println!("removed: {:?}", one);
 
-    let two = hm.remove_entry(&10);
-    println!("removed: {:?}", two);
+    // let two = hm.remove_entry(&10);
+    // println!("removed: {:?}", two);
 
-    hm.clear();
-    println!("is empty? {}", hm.is_empty());
+    // hm.clear();
+    // println!("is empty? {}", hm.is_empty());
+
+    let mut hs = HashSet::new();
+
+    hs.insert(2);
+    hs.insert(22);
+    hs.insert(12);
+    hs.insert(5);
+    hs.insert(2);
+    hs.insert(2);
+    hs.insert(11);
+
+    println!("{:?}", hs);
+
+    for x in hs.iter() {
+        println!("iter: {}", x);
+    }
+
+    let mut hs2 = HashSet::new();
+    hs2.insert(99);
+    hs2.insert(22);
+    hs2.insert(15);
+    hs2.insert(11);
+
+    for x in hs.intersection(&hs2) {
+        println!("common: {}", x);
+    }
+
+    let union = &hs | &hs2;
+    for x in union {
+        println!("union: {}", x);
+    }
+
+    let diff = &hs - &hs2;
+    for x in diff {
+        println!("diff: {}", x);
+    }
 }

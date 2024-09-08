c5d03c4850b1cdf2a23a7f6b765bf22b05f3f9c4
arshkhatwani <arsh.khatwani@gmail.com>
2024-09-09 01:03:18 +0530

    Binary Heap

--- section_8/src/main.rs
+++ section_8/src/main.rs
@@ -1,27 +1,42 @@
+use std::collections::BinaryHeap;
+
 use rand::seq::SliceRandom;
 
 fn main() {
-    let mut nums: Vec<i32> = vec![];
+    // let mut nums: Vec<i32> = vec![];
 
-    nums.push(21);
-    nums.push(3);
-    nums.push(32);
-    nums.push(55);
-    nums.push(14);
-    println!("{:?}", nums);
+    // nums.push(21);
+    // nums.push(3);
+    // nums.push(32);
+    // nums.push(55);
+    // nums.push(14);
+    // println!("{:?}", nums);
 
-    nums.pop();
-    println!("{:?}", nums);
+    // nums.pop();
+    // println!("{:?}", nums);
 
-    nums.insert(1, 100);
-    println!("{:?}", nums);
+    // nums.insert(1, 100);
+    // println!("{:?}", nums);
 
-    nums.sort();
-    println!("After sort: {:?}", nums);
+    // nums.sort();
+    // println!("After sort: {:?}", nums);
 
-    nums.reverse();
-    println!("After reverse: {:?}", nums);
+    // nums.reverse();
+    // println!("After reverse: {:?}", nums);
 
-    nums.shuffle(&mut rand::thread_rng());
-    println!("After shuffle: {:?}", nums);
+    // nums.shuffle(&mut rand::thread_rng());
+    // println!("After shuffle: {:?}", nums);
+
+    let mut bheap = BinaryHeap::new();
+
+    bheap.push(1);
+    bheap.push(2);
+    bheap.push(4);
+    bheap.push(5);
+    bheap.push(1);
+
+    println!("{:?}", bheap);
+
+    let el = bheap.peek();
+    println!("{:?}", el);
 }

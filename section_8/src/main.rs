2774fce42edc933d654706bfe1cb6fdf2264575c
arshkhatwani <arsh.khatwani@gmail.com>
2024-09-09 01:18:58 +0530

    Maps

--- section_8/src/main.rs
+++ section_8/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::BinaryHeap;
+use std::collections::{BinaryHeap, HashMap};
 
 use rand::seq::SliceRandom;
 
@@ -27,16 +27,40 @@ fn main() {
     // nums.shuffle(&mut rand::thread_rng());
     // println!("After shuffle: {:?}", nums);
 
-    let mut bheap = BinaryHeap::new();
+    // let mut bheap = BinaryHeap::new();
 
-    bheap.push(1);
-    bheap.push(2);
-    bheap.push(4);
-    bheap.push(5);
-    bheap.push(1);
+    // bheap.push(1);
+    // bheap.push(2);
+    // bheap.push(4);
+    // bheap.push(5);
+    // bheap.push(1);
 
-    println!("{:?}", bheap);
+    // println!("{:?}", bheap);
 
-    let el = bheap.peek();
-    println!("{:?}", el);
+    // let el = bheap.peek();
+    // println!("{:?}", el);
+
+    let mut hm = HashMap::new();
+
+    hm.insert(2, 3);
+    hm.insert(3, 6);
+    let old = hm.insert(2, 5);
+    hm.insert(10, 8);
+
+    println!("{:?}", hm);
+    println!("old val: {:?}", old);
+
+    println!("has key? {}", hm.contains_key(&10));
+    println!("has key? {}", hm.contains_key(&100));
+    println!("find {:?}", hm.get(&10));
+    println!("find {:?}", hm.get(&100));
+
+    let one = hm.remove(&2);
+    println!("removed: {:?}", one);
+
+    let two = hm.remove_entry(&10);
+    println!("removed: {:?}", two);
+
+    hm.clear();
+    println!("is empty? {}", hm.is_empty());
 }

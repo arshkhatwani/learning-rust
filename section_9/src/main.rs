0576325bc20a508fe30cf49add67a7888b57fd5b
arshkhatwani <arsh.khatwani@gmail.com>
2024-09-10 12:42:53 +0530

    Result

--- section_9/src/main.rs
+++ section_9/src/main.rs
@@ -1,8 +1,19 @@
-fn main() {
-    println!("Hello, world!");
-    // panic!("something happened!"); // also occurs when something is accessed outside bound of vector
-    println!("Hello, world!2");
+use std::fs::File;
 
-    let a = vec![1, 2, 3];
-    println!("{}", a[3]); // panic is expected here
+fn main() {
+    // println!("Hello, world!");
+    // // panic!("something happened!"); // also occurs when something is accessed outside bound of vector
+    // println!("Hello, world!2");
+
+    // let a = vec![1, 2, 3];
+    // println!("{}", a[3]); // panic is expected here
+
+    // let file = File::open("error.txt");
+    // the above should return a 'Result'
 }
+
+// Result looks something like this
+// enum Result<T, E> {
+//     Ok(T),
+//     Err(E)
+// }

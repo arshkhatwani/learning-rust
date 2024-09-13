382f72c81f42a2fd0c72bd10981771022483ec5c
arshkhatwani <arsh.khatwani@gmail.com>
2024-09-13 12:24:38 +0530

    Assertations

--- section_10/src/lib.rs
+++ section_10/src/lib.rs
@@ -1,5 +1,7 @@
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
         let result = 2 + 2;
@@ -7,7 +9,25 @@ mod tests {
     }
 
     #[test]
+    #[should_panic] // expecting panic
     fn it_fails() {
         panic!("test failed -- test case not passed");
+    }
+
+    #[test]
+    fn call_simple_add() {
+        assert!(simple_add())
+    }
+}
+
+// use 'cargo test' command to execute tests
+// use 'cargo test call_simple_add' to execute a particular test
+// use 'cargo test it' to run tests starting from it
+
+fn simple_add() -> bool {
+    if 2 + 2 == 4 {
+        true
+    } else {
+        false
     }
 }
